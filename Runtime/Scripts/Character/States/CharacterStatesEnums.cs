ee1fddc8681074164aeef8c17c02f144c4aa306a
crowhound <jonathan.westfall1@gmail.com>
2023-04-16 23:26:06 -0500

    Created the Character States Enums for movement and character states

---
+++ Runtime/Scripts/Character/States/CharacterStatesEnums.cs
@@ -0,0 +1,20 @@
+namespace SF.Characters
+{
+    public enum CharacterState
+    {
+        Normal,
+        Alive,
+        Dead
+    }
+    public enum MovementState
+    {
+        Idle,
+        Walking,
+        Running,
+        Falling, 
+        Jumping,
+        Swimming,
+        WallSliding,
+        WallJumping
+    }
+}
