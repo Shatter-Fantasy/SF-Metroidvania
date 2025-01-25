cc63c91d5255cc96b7377a6e3a61032c88b4bbac
crowhound <jonathan.westfall1@gmail.com>
2025-01-25 17:53:37 -0600

    Created a base class for data objects and data scriptable objects.
    Added:
    - DTOBase is for normal class data and is used when not needing to save/load data in editor like database editing.
    
    - DTOAssetBase  is for Unity asset like DTO objects like scriptableobjects to use in editor databases.

---
+++ Runtime/Scripts/Data Core/DTOBase.cs
@@ -0,0 +1,40 @@
+using UnityEditor;
+
+using UnityEngine;
+
+namespace SF
+{
+
+
+    /// <summary>
+    /// This is an empty wrapper class for DTO based classes.
+    /// 
+    /// Example of this wrapper being utilized can be seen in the abstract DataView class that takes in a generic type that inherits from DTOBase.
+    /// <seealso cref="SF.Inventory.ItemGeneralDTO"/>
+    /// <seealso cref="SF.Inventory.ItemPriceDTO"/>
+    /// </summary>
+    public class DTOBase 
+    {
+        public int ID = 0;
+        public string GUID;
+        public string Name;
+        public string Description;
+    }
+
+    /// <summary>
+    /// This is a DTObase that can be used as a scriptable object where needed.
+    /// </summary>
+    public class DTOAssetBase : ScriptableObject
+    {
+        public int ID = 0;
+        public int GUID;
+        public string Name;
+        public string Description;
+
+        public DTOAssetBase() 
+        {
+            GUID = this.GetInstanceID();
+        }
+    }
+
+}
