b8be28b2c157b3b92c7c7cb76ce442410fc703c3
crowhound <jonathan.westfall1@gmail.com>
2025-01-25 17:56:01 -0600

    Added a temp stat list before finishing the stat mediator.
    Added:
    - The temp StatList will be changed to use the stat mediator in the future.
    For now using it to allow characters to have test for early game play testing.

---
+++ Runtime/Scripts/Stat System/StatList.cs
@@ -0,0 +1,15 @@
+namespace SF.StatModule
+{
+    [System.Serializable] 
+    public class StatList
+    {
+        public int HP;
+        public int Soule;
+
+        public int PhysicalAttack;
+        public int PhysicalDefence;
+
+        public int MagicalAttack;
+        public int MagicalDefence;
+    }
+}
