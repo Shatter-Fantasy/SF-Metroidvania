a06baaffda80fcfdec8570d4febf5e6f7d39c0dc
crowhound <jonathan.westfall1@gmail.com>
2025-02-07 13:45:32 -0600

    Added base class for the component ChartacterStats
    Ignore Changelog:
    Note with the new implicit override I am making we might not need this class. We might be able to just have the character data component used for gameobjects just take in the data of the CharacterDTO from database.

---
+++ Runtime/Scripts/Stat System/CharacterStats.cs
@@ -0,0 +1,39 @@
+using SF.DamageModule;
+using SF.SpawnModule;
+
+using UnityEngine;
+
+namespace SF.StatModule
+{
+
+
+    /// <summary>
+    /// Links the character stats from the database to an actual gameobject. Also controls stat events for external components relying on character data. Example linking HP stat to a health controller script.
+    /// </summary>
+    public class CharacterStats : MonoBehaviour, IDamageController
+    {
+        public StatList CharacterStatList;
+
+        /// <summary>
+        /// The external character health component to link to.
+        /// </summary>
+        [SerializeField] CharacterHealth _characterHealth;
+
+        public int CalculateDamage(int preDamage)
+        {
+            return preDamage;
+        }
+
+        private void Awake()
+        {
+            _characterHealth  =  _characterHealth != null 
+                ? _characterHealth 
+                : GetComponent<CharacterHealth>();
+
+            // Make sure a component was find just in case the above was null.
+            // IUf it is not null tell it is being externally controlled.
+            if(_characterHealth != null)
+                _characterHealth.DamageController = this;
+        }
+    }
+}
