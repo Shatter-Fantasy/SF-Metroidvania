36f3c5aa24a6368cb55fd212cf3e24cbd6f6a47f
crowhound <jonathan.westfall1@gmail.com>
2025-02-07 13:50:18 -0600

    Created the CombatantHealth script for characters that are in combat in game that can drop items from a loottable.

---
+++ Runtime/Scripts/Health System/CombatantHealth.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+using SF.Characters;
+using SF.ItemModule;
+
+namespace SF.SpawnModule
+{
+    public class CombatantHealth : CharacterHealth
+    {
+        private CombatantData _combatantData;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _combatantData = GetComponent<CombatantData>();
+        }
+
+        protected override void Kill()
+        {
+            if (_combatantData != null 
+                && _combatantData.EnemyLootTable.LootTable != null 
+                && _combatantData.EnemyLootTable.LootTable.Count > 0)
+            {
+                GameObject spawnedItem = Instantiate(
+                    _combatantData.EnemyLootTable.LootTable[0].Prefab,
+                    transform.position,
+                    Quaternion.identity
+                );
+
+                spawnedItem.GetComponent<PickupItem>().Item = _combatantData.EnemyLootTable.LootTable[0];
+            }
+
+            base.Kill();
+        }
+    }
+}
