2fb428f41d3baec8bbd9d04f2b5269c80edf87ab
crowhound <jonathan.westfall1@gmail.com>
2025-02-07 13:14:29 -0600

    Implemented the StatList class to replace the previously manually add list of stat mediators.
    Changed:
    - Implemented the new StatList class in the StatDatabase. This will be used to set the default list of possible stats for characters, items, and so forth.

--- Runtime/Scripts/Stat Module/Core/StatDatabase.cs
+++ Runtime/Scripts/Stat Module/Core/StatDatabase.cs
@@ -14,26 +14,16 @@ namespace SF.StatModule
     /// Example 1. All characters have the same set of attributes.
     /// Strength, agility, vitility, and so forth.
     /// </remarks>
-    [CreateAssetMenu(fileName = "Stat System Database", menuName = "SF/Stats/Stat Database")]
+    [CreateAssetMenu(fileName = "Stat System Database", menuName = "SF/StatMediators/Stat Database")]
     public partial class StatSetDatabase : ScriptableObject
     {
         // Important Information. This class is partial to allow for people to add their own types of Stat Sets
         // It also let's them add their own type casting overloads properties.
-        public List<StatMediator<RPGAttributeStat>> AttributesStatSet = new List<StatMediator<RPGAttributeStat>>();
 
-        /// <summary>
-        /// This is the default stats for characters used in combat.
-        /// Think physical damage, magical defence, and so forth.
-        /// </summary>
-        public List<StatMediator<StatData>> CombatStats = new List<StatMediator<StatData>>();
-        public List<StatMediator<StatData>> EquipmentStatSet = new List<StatMediator<StatData>>();
+        public StatList DefaultStatTypes;
 
-        public List<StatMediator<ElementalAttributeStat>> ElementalPowerStats = new List<StatMediator<ElementalAttributeStat>>().SetDefaultElementalStats();
-        public List<StatMediator<ElementalAttributeStat>> ElementalResistanceStats = new List<StatMediator<ElementalAttributeStat>>().SetDefaultElementalStats();
-        public List<StatMediator<ElementalAttributeStat>> ElementalAffinityStats = new List<StatMediator<ElementalAttributeStat>>().SetDefaultElementalStats();
-
+        [Space(10)]
         public List<StatMediator<PricingStat>> PriceStats = new List<StatMediator<PricingStat>>();
-
     }
 
 }
