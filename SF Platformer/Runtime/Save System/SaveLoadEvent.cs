71bc776495eacee37225562301927a03f0f12988
crowhound <jonathan.westfall1@gmail.com>
2025-03-30 13:27:07 -0500

    Added a SaveLoadEvent struct for managing events related to save files and data management.

---
+++ SF Platformer/Runtime/Save System/SaveLoadEvent.cs
@@ -0,0 +1,31 @@
+using SF.Events;
+using UnityEngine;
+
+namespace SF.DataManagement
+{
+
+    public enum SaveLoadEventTypes
+    {
+        Saving,
+        Loading,
+        CopyingFile,
+        DeletingSaveFile
+    }
+    public struct SaveLoadEvent
+    {
+        public SaveLoadEventTypes EventType;
+        
+        public SaveLoadEvent(SaveLoadEventTypes eventType)
+        {
+            EventType = eventType;
+        }
+        
+        static SaveLoadEvent saveLoadEvent;
+
+        public static void Trigger(SaveLoadEventTypes eventType)
+        {
+            saveLoadEvent.EventType = eventType;
+            EventManager.TriggerEvent(saveLoadEvent);
+        }
+    }
+}
