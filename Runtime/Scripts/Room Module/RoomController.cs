55bb5729c3fc24c09df98e19628b55a06d50e159
crowhound <jonathan.westfall1@gmail.com>
2025-04-24 18:26:18 -0500

    Moved the RoomController into the Metroidvania assembly.

---
+++ Runtime/Scripts/Room Module/RoomController.cs
@@ -0,0 +1,82 @@
+using System;
+using UnityEngine;
+
+namespace SF.RoomModule
+{
+    public class RoomController : MonoBehaviour
+    {
+        public int RoomID;
+        public BoxCollider2D RoomConfiner;
+
+        private void Awake()
+        {
+            if (RoomConfiner == null)
+                RoomConfiner = GetComponent<BoxCollider2D>();
+        }
+
+        public void LoadRoom()
+        {
+            RoomSystem.LoadRoom(RoomID);
+        }
+        
+        
+        private static Vector3 StartMousePosition;
+        private static Vector3 EndMousePosition;
+        Rect tRect = new Rect(100,0,250,250);
+        Rect boundaryRect = new Rect(100,0,250,250);
+
+        /*void OnGUI()
+        {
+           
+            Event current = Event.current;
+
+            switch(current.type)
+            {
+                case EventType.MouseDown:
+                    {
+                        StartMousePosition = current.mousePosition;
+                        current.Use();
+                        break;
+                    }
+                case EventType.MouseDrag:
+                    {
+                        EndMousePosition = current.mousePosition;
+                        boundaryRect = RectByVectorEdges(StartMousePosition, EndMousePosition);
+                        current.Use();
+                        break;
+                    }
+                case EventType.MouseUp:
+                    {
+                        EndMousePosition = current.mousePosition;
+                        boundaryRect = RectByVectorEdges(StartMousePosition, EndMousePosition);
+                        current.Use();
+                        break;
+                    }
+                case EventType.Repaint:
+                    {
+                        GL.Begin(GL.LINES);
+                        GLUtilities.InitHandleMaterial();
+                        GLUtilities.ApplyHandleMaterial();
+                        GLUtilities.StartDrawing(Matrix4x4.identity, Color.red);
+                        GLUtilities.DrawBox(boundaryRect);
+                        GLUtilities.DrawBox(tRect);
+
+                        GLUtilities.EndDrawing();
+                        break;
+                    }
+            }
+        }
+
+        public Rect RectByVectorEdges(Vector2 leftCorner, Vector2 rightCorner)
+        {
+            var centerPoint = (leftCorner + rightCorner) / 2f;
+            var size = new Vector2(
+                    Mathf.Abs(leftCorner.x - rightCorner.x),
+                    Mathf.Abs(leftCorner.y - rightCorner.y)
+                );
+
+            return new Rect(centerPoint, size);
+        }*/
+
+    }
+}
