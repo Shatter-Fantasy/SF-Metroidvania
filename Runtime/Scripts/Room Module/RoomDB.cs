0ce45172de779b0a08f9a515a084b79bc6de0c2f
crowhound <jonathan.westfall1@gmail.com>
2025-04-24 18:27:15 -0500

    Created a scriptable object class that will act like the Room Database for the game.

---
+++ Runtime/Scripts/Room Module/RoomDB.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF.RoomModule
+{
+    [CreateAssetMenu(fileName = "Room DB", menuName = "SF/Data/Rooms/Room Database")]
+    public class RoomDB : ScriptableObject , IList<Room>
+    {
+
+        public static RoomDB Instance;
+        
+        public List<Room> Rooms = new();
+        
+        private void OnEnable()
+        {
+            if (Instance == null)
+                Instance = this;
+        }
+
+        public IEnumerator<Room> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(Room item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool Contains(Room item)
+        {
+            return true;
+        }
+
+        public void CopyTo(Room[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Room item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+        public int IndexOf(Room item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, Room item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Room this[int index]
+        {
+            get => Rooms.Find(x => x.RoomID == index);
+            set => throw new NotImplementedException();
+        }
+    }
+}
