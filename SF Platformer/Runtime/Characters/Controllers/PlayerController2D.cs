4c05cedfaac1c363169f4ba0de0092162032bf6a
crowhound <jonathan.westfall1@gmail.com>
2025-04-24 18:31:14 -0500

    Added a temp set up where the PlayerController will assign the  GameManager.Instance.PlayerController OnAwake.

--- SF Platformer/Runtime/Characters/Controllers/PlayerController2D.cs
+++ SF Platformer/Runtime/Characters/Controllers/PlayerController2D.cs
@@ -1,19 +1,22 @@
 using Unity.Cinemachine;
-
 using SF.CameraModule;
+using SF.Managers;
 using SF.Utilities;
+
+
 namespace SF.Characters.Controllers
 {
 	public class PlayerController : GroundedController2D
     {
-
         protected override void OnAwake()
         {
             base.OnAwake();
 
             if(gameObject.TryGetComponentInChildren(out CinemachineCamera cinemachineCamera))
                 CameraController.SetPlayerCMCamera(cinemachineCamera);
+
+            if (GameManager.Instance != null && GameManager.Instance.PlayerController == null)
+                GameManager.Instance.PlayerController = this;
         }
     }
-    
 }
