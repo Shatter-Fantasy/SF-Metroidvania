e198205491235cf6b32b71abf52a22b6e381af3a
crowhound <jonathan.westfall1@gmail.com>
2025-04-24 18:28:25 -0500

    This is temporary, but made a Regions enum.
    Just for now made a Region enum with IC based areas. Will change this in a bit to allow custom user regions, but needed it for now because of a demo.

---
+++ Runtime/Scripts/Room Module/Regions/Regions.cs
@@ -0,0 +1,12 @@
+namespace SF.RoomModule
+{
+    public enum Regions : int
+    {
+        CastleCorridor = 0,
+        HallOfLingering = 1,
+        TowerOfCelsestiaAscension = 2,
+        CastleLibrary = 4,
+        MermaidHaven,
+        GaiaHeart
+    }
+}
