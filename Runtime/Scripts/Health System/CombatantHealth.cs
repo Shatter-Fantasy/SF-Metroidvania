a8d91014505d2e257765a3fdf8b27068f7b61d8c
crowhound <jonathan.westfall1@gmail.com>
2025-05-18 12:03:51 -0500

    Fixed: Enemies were not giving experience when they didn't have a loot table assigned to them with a single loot in it.
    Fixed: Made an internal if statement for making sure we give loot without ignoring experience handling.

--- Runtime/Scripts/Health System/CombatantHealth.cs
+++ Runtime/Scripts/Health System/CombatantHealth.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 using SF.Characters.Data;
@@ -20,22 +19,25 @@ namespace SF.SpawnModule
         
         protected override void Kill()
         {
-            if (_combatantData is not null 
-                && _combatantData.EnemyLootTable is not null 
-                && _combatantData.EnemyLootTable.LootTable.Count > 0)
+            if (_combatantData is not null)
             {
                 // TODO: Will need checks later for allies and summonings to not grant experience.
                 //  Grant the player his expierence from the enemy kill.
                 
                 ExperienceEvent.Trigger(ExperienceEventTypes.Gain,_combatantData.Experience.BaseExperience);
-                
-                GameObject spawnedItem = Instantiate(
-                    _combatantData.EnemyLootTable.LootTable[0].Prefab,
-                    transform.position,
-                    Quaternion.identity
-                );
 
-                spawnedItem.GetComponent<PickupItem>().Item = _combatantData.EnemyLootTable.LootTable[0];
+                // Loot Drop checks.
+                if (_combatantData.EnemyLootTable is not null
+                    && _combatantData.EnemyLootTable.LootTable.Count > 0)
+                {
+                    GameObject spawnedItem = Instantiate(
+                        _combatantData.EnemyLootTable.LootTable[0].Prefab,
+                        transform.position,
+                        Quaternion.identity
+                    );
+
+                    spawnedItem.GetComponent<PickupItem>().Item = _combatantData.EnemyLootTable.LootTable[0];
+                }
             }
 
             base.Kill();
