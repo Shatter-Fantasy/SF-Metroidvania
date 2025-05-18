dda7b481b64e4401a1bbceef6b21ca0c7bc95ff5
crowhound <jonathan.westfall1@gmail.com>
2025-05-18 12:12:47 -0500

    Added the ability to stop timers and not just restart them.
    Notes:
    When a Timer is stopped the _onTimerComplete will still play. I might switch it to have a OnTimerInterupted action to be invoked instead to allow for clean up when Timers are interrupted.

--- SF Platformer/Runtime/Utilities/Timing/Timer.cs
+++ SF Platformer/Runtime/Utilities/Timing/Timer.cs
@@ -7,12 +7,14 @@ namespace SF
     public class Timer
     {
         public float StartingTime = 3;
-        public float RemaingTime;
+        public float RemainingTime;
 
         /// <summary>
         /// An Action that is called at the end when the Timer hits 0.
         /// </summary>
         private Action _onTimerComplete;
+
+        private bool _timerStopped;
         public Timer(Action onTimerComplete = null)
         {
             if (onTimerComplete == null)
@@ -23,11 +25,12 @@ namespace SF
         public Timer(float startingTime, Action onTimerComplete = null) : this(onTimerComplete)
         {
             StartingTime = startingTime;
-            RemaingTime = startingTime;
+            RemainingTime = startingTime;
         }
         public void ResetTimer()
         {
-            RemaingTime = StartingTime;
+            _timerStopped = false;
+            RemainingTime = StartingTime;
         }
         /// <summary>
         /// Starts an async timer that when completed raises the 
@@ -43,18 +46,26 @@ namespace SF
             return true;
         }
 
+        public void StopTimer()
+        {
+            _timerStopped = true;
+        }
+
         /// <summary>
         /// Tells the timer to start updating and counting down asynchronously and invokes the <see cref="_onTimerComplete"/> event when the timer reaches zero.
         /// </summary>
         /// <returns></returns>
         public async Awaitable UpdateTimerAsync()
         {
-            while (RemaingTime > 0)
+            while (RemainingTime > 0)
             {
+                if(_timerStopped)
+                    break;
+                
                 await Awaitable.EndOfFrameAsync();
-                RemaingTime -= Time.deltaTime;
+                RemainingTime -= Time.deltaTime;
             }
-            RemaingTime = 0;
+            RemainingTime = 0;
             _onTimerComplete?.Invoke();
         }
     }
