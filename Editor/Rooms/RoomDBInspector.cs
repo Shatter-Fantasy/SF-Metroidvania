e45ca5a9deb0c7e07f0e0c4df51ea2afb2fa1b04
crowhound <jonathan.westfall1@gmail.com>
2025-06-10 13:59:41 -0500

    When we add a new room to the RoomDatabase it will auto set the RoomId on creation in the room list.

--- Editor/Rooms/RoomDBInspector.cs
+++ Editor/Rooms/RoomDBInspector.cs
@@ -1,8 +1,8 @@
+using System.Collections.Generic;
 using SF.RoomModule;
 using SF.UIElements.Utilities;
 using UnityEditor;
 using UnityEditor.UIElements;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace SFEditor.Rooms
@@ -10,21 +10,43 @@ namespace SFEditor.Rooms
     [CustomEditor(typeof(RoomDB))]
     public class RoomDBInspector : Editor
     {
+        private VisualElement _root;
+        private ListView _roomListView;
+        private RoomDB _roomDB;
+        
         public override VisualElement CreateInspectorGUI()
         {
-            var root = new VisualElement();
-            InspectorElement.FillDefaultInspector(root,serializedObject,this);
-            root.AddChild(new Button(SetRoomDBInstance){ text = "Set Instance" });
-            root.AddChild(new Button(LogRoomInstances){ text = "Log RoomDB Instances" });
-            root.AddChild(new Button(SetRoomIDs){ text = "Set Room Prefab IDs" });
-            return root;
+            _roomDB = target as RoomDB;
+            _root = new VisualElement() {name = "room-db--root"};
+            InspectorElement.FillDefaultInspector(_root,serializedObject,this);
+
+
+            _root.AddChild(new Button(SetRoomDBInstance){ text = "Set Instance" });
+            _root.AddChild(new Button(SetRoomIDs){ text = "Set Room Prefab IDs" });
+            
+            _root.RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+            
+            return _root;
         }
 
-        private void LogRoomInstances()
+        private void OnAttachToPanel(AttachToPanelEvent evt)
         {
-            Debug.Log($"RoomSystem.RoomDB value is: { RoomSystem.RoomDB } and the RoomDB.Instance value is {RoomDB.Instance}.");
+            this.
+            // Unity auto assigns a name to the list-view based on the name of the variable.
+            _roomListView = _root.Q<ListView>("unity-list-Rooms");
+            if (_roomListView != null)
+            {
+                _roomListView.itemsAdded += OnRoomAdded;
+            }
         }
 
+        private void OnRoomAdded(IEnumerable<int> roomsAdded)
+        {
+            foreach (int roomIndex in roomsAdded)
+            {
+                _roomDB[roomIndex].RoomID = roomIndex;
+            }
+        }
 
         private void SetRoomDBInstance()
         {
@@ -38,16 +60,7 @@ namespace SFEditor.Rooms
         
         private void SetRoomIDs()
         {
-            var roomDB = target as RoomDB;
-
-            if (roomDB == null)
-                return;
-            
-            foreach (Room room in roomDB.Rooms)
-            {
-                // These are the prefab assets inside of the project folder.
-                room.RoomPrefab.GetComponent<RoomController>().RoomID = room.RoomID;
-            }
+            _roomDB.ResetRoomIds();
         }
     }
 }
