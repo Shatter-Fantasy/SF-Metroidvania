21303e282882dfe5dd39e178538bc004565d34bf
crowhound <jonathan.westfall1@gmail.com>
2025-06-14 23:00:12 -0500

    CheckpointManager no longer is responsible for calling a respawn event on Start. The GameLoader now takes care of that when the game runtime is loading a scene.

--- Runtime/Scripts/CheckPoint/CheckPointManager.cs
+++ Runtime/Scripts/CheckPoint/CheckPointManager.cs
@@ -6,11 +6,6 @@ namespace SF.SpawnModule
 {
     public class CheckPointManager : MonoBehaviour, EventListener<CheckPointEvent>
     {
-		/// <summary>
-		/// Should the the player spawn at the starting check point at the start of playing a level if the starting checkpoint is not null.
-		/// </summary>
-		[SerializeField] private bool _useStartingCheckPoint = true;
-
         public CheckPoint StartingCheckPoint;
         public CheckPoint CurrentCheckPoint;
 
@@ -39,17 +34,8 @@ namespace SF.SpawnModule
         }
         private void Start()
         {
-			// If we have no starting check point set try to find one.
-			if(StartingCheckPoint == null)
-				StartingCheckPoint = FindFirstObjectByType<CheckPoint>();
-
-			
 			if(StartingCheckPoint != null && CurrentCheckPoint == null)
 				CurrentCheckPoint = StartingCheckPoint;
-
-			if(_useStartingCheckPoint && StartingCheckPoint != null)
-                RespawnEvent.Trigger(RespawnEventTypes.PlayerRespawn);
-
         }
         public void OnEvent(CheckPointEvent checkPointEvent)
 		{
