e6994c31130e15b51c1335cf6a89f174bda6cf37
crowhound <jonathan.westfall1@gmail.com>
2025-07-06 13:10:20 -0500

    Started doing some misc updates to Room Editors.

--- Runtime/Scripts/Character Spawners/RoomCharacterSpawner.cs
+++ Runtime/Scripts/Character Spawners/RoomCharacterSpawner.cs
@@ -153,10 +153,17 @@ namespace SF.SpawnModule
             {
                 EditorGUI.BeginChangeCheck();
                 Vector3 newTargetPosition = Handles.PositionHandle(t.SpawnSets[i].SpawnPosition, Quaternion.identity);
-                
-                if(t.CharacterDB != null && t.CharacterDB.GetDataByID(t.SpawnSets[i].SpawnCharacterID, out CharacterDTO characterDTO))
-                    Handles.Label(newTargetPosition,$"{characterDTO.name }");
-                
+
+                if (t.CharacterDB != null &&
+                    t.CharacterDB.GetDataByID(t.SpawnSets[i].SpawnCharacterID, out CharacterDTO characterDTO))
+                {
+                    Handles.Label(newTargetPosition, $"{characterDTO.name}");
+                    if (characterDTO.Prefab?.GetComponent<SpriteRenderer>() != null)
+                    {
+                        
+                    }
+                }
+
                 if (EditorGUI.EndChangeCheck())
                 {
                     Undo.RecordObject(t, "Change Look At Target Position");
