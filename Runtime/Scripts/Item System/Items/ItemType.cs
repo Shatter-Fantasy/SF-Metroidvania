38402afe48692c01037876cb9fa521caeebd8337
crowhound <jonathan.westfall1@gmail.com>
2025-07-12 15:22:49 -0500

    Removed old ItemType enum completely.

--- Runtime/Scripts/Item System/Items/ItemType.cs
+++ Runtime/Scripts/Item System/Items/ItemType.cs
@@ -10,17 +10,4 @@ namespace SF.ItemModule
         Equipment,
         None // This is used for when filtering items in different places.
     }
-    
-    /// <summary>
-    /// IMPORTANT: This will be removed. Do not use this from now on.
-    ///  Use <see cref="ItemSubType"/> from now on.
-    /// </summary>
-    [System.Obsolete]
-    public enum ItemType
-    {
-        Consumable,
-        Weapon,
-        Armor,
-        Key
-    }
 }
