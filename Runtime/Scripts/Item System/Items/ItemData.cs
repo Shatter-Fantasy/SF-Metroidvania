36bc8755249de31766476f910728f8d9c552e37c
crowhound <jonathan.westfall1@gmail.com>
2025-07-13 11:27:04 -0500

    Added some item sub types for armor and weapons. Also added some constructors for ItemData.

--- Runtime/Scripts/Item System/Items/ItemData.cs
+++ Runtime/Scripts/Item System/Items/ItemData.cs
@@ -3,20 +3,44 @@ using SF.Inventory;
 namespace SF.ItemModule
 {
     /// <summary>
-    /// IMPORTANT: At the moment the <see cref="ItemGeneralDTO"/> does something similar to this.
-    /// I need to make them into a single class for the future, instead of needing two classes.
+    /// The representation of an item and its data sent between objects. 
     /// </summary>
+    /// <remarks>
+    /// The <see cref="ItemDTO"/> is the actual data asset in the project, while <see cref="ItemData"/> is used for
+    /// passing the data that represents an item between objects.
+    /// </remarks>
     [System.Serializable]
     public class ItemData
     {
-        // TODO: IMPORTANT: At the moment the <see cref="ItemGeneralDTO"/> does something similar to this.
-        //  I need to make them into a single class for the future, instead of needing two classes.
-        
         public int ID = 0;
-        public string Name = "New Item";
-        public string Description = "New Description";
+        public string Name;
+        public string Description;
 
         public ItemSubType ItemSubType;
+        
+        public ItemData() : this("New Item") { }
+        public ItemData(string name = "New Item", string description = "New Item", int id = -1)
+        {
+            Name = name;
+            Description = description;
+            ID = id;
+        }
+
+        public virtual void Use() { }
+        
+        /* TODO: Implement ovverride for GetHashCode and Equal(Object)
+        public static bool operator ==(ItemData item1, ItemData item2)
+        {
+            if (item1 is null || item2 is null)
+                return false;
+            
+            return item1.ID == item2.ID;
+        }
+        public static bool operator !=(ItemData item1, ItemData item2)
+        {
+            return !(item1 == item2);
+        }
+        */
         
         public static implicit operator ItemData(ItemDTO itemAsset) 
         {
