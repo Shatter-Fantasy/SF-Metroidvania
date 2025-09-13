8046812cdf26b4ebc9d37d5ddb8feab51df1dcb8
8BitsPerByte <jonathan.westfall1@gmail.com>
2025-09-13 15:53:04 -0500

    Switch LayerMask to use a ContactFilter for SightDetection. This is for some Unity 6.3 API changes I will be implementing soon.

--- Runtime/Scripts/Detection Module/SightDetectionDecision.cs
+++ Runtime/Scripts/Detection Module/SightDetectionDecision.cs
@@ -1,3 +1,4 @@
+using SF.Characters.Controllers;
 using SF.StateMachine.Core;
 
 using UnityEngine;
@@ -11,17 +12,25 @@ namespace SF.StateMachine.Decisions
     public class SightDetectionDecision : StateDecisionCore
     {
         [SerializeField] private float _sightDistance = 4;
-        [SerializeField] private LayerMask _detectionLayer;
+        [SerializeField] private ContactFilter2D _detectionFilter;
 
+        private readonly RaycastHit2D[] _filteredHits = new RaycastHit2D[1];
+        private Controller2D _controller2D;
+        
+        protected override void Init()
+        {
+            if (TryGetComponent(out StateMachineBrain brain)
+                && brain.ControlledGameObject.TryGetComponent(out _controller2D)) ;
+        }
 
         public override void CheckDecision(ref DecisionTransition decision, StateCore currentState)
         {
-           if(Physics2D.Raycast(transform.position, Vector2.right, _sightDistance, _detectionLayer))
-           {
+            if(Physics2D.Raycast(transform.position, _controller2D.Direction, _detectionFilter,_filteredHits,_sightDistance) > 0)
+            {
                 decision.CanTransist = true;
                 decision.StateGoingTo = _trueState;
-           }
-           else
+            }
+            else
             {
                 decision.CanTransist = true;
                 decision.StateGoingTo = _falseState;
