3a842bd26bae0e70e79179d4d7aa6a9e4b8facb6
8BitsPerByte <jonathan.westfall1@gmail.com>
2025-09-13 15:58:58 -0500

    Started the WIP Drop Chance system for Loot Tables when enemies or containers get killed or destroyed.

--- Runtime/Scripts/Loot System/LootTableData.cs
+++ Runtime/Scripts/Loot System/LootTableData.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 using SF.Inventory;
 using SF.DataModule;
+using SF.ItemModule;
 
 namespace SF.LootModule
 {
@@ -10,6 +11,56 @@ namespace SF.LootModule
     public class LootTableData : DTOAssetBase
     {
         public List<ItemDTO> LootTable = new List<ItemDTO>();
-    }
+        
+        /// <summary>
+        /// The drop chance in percent.
+        /// </summary>
+        public float DropChance = 0.5f; 
+        public void DropRandomLoot(Vector2 dropPosition)
+        {
+            if (LootTable.Count < 1 
+                || Random.Range(0f,1f) < DropChance)
+                return;
 
+            int lootIndex = Random.Range(0, LootTable.Count);  
+            
+            var item = LootTable[lootIndex];
+
+            if (item == null)
+                return;
+            
+            // Default object that allows for not setting the prefab on all scriptable data assets. 
+            if (item.Prefab == null)
+            {
+                GameObject spawnedItem = new GameObject(
+                    item.Name,
+                    typeof(SpriteRenderer),
+                    typeof(PickupItem)
+                )
+                {
+                    transform = { position = dropPosition }
+                };
+
+                spawnedItem.GetComponent<SpriteRenderer>().sprite = item.ItemIcon;
+                spawnedItem.GetComponent<PickupItem>().Item = item;
+                
+                // We add the box collider after setting the sprite to make sure the size of the collider is set properly.
+                var col = spawnedItem.AddComponent<BoxCollider2D>();
+                col.isTrigger = true;
+                
+            }
+            else
+            {
+                GameObject spawnedItem = Instantiate(
+                    item.Prefab,
+                    dropPosition,
+                    Quaternion.identity
+                );
+                
+                spawnedItem.name = item.Name;
+                spawnedItem.GetComponent<PickupItem>().Item = item;
+                spawnedItem.GetComponent<SpriteRenderer>().sprite = item.ItemIcon;
+            }
+        }
+    }
 }
