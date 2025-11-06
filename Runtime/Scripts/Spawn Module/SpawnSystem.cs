4eeb50a330d7472c84a067a9310f283cabe8439b
8BitsPerByte <jonathan.westfall1@gmail.com>
2025-11-06 16:04:59 -0600

    Added the start of the core Spawn System to help control the logic of the player spawning in game.

---
+++ Runtime/Scripts/Spawn Module/SpawnSystem.cs
@@ -0,0 +1,42 @@
+using System;
+using SF.PhysicsLowLevel;
+using SF.RoomModule;
+using UnityEngine;
+
+namespace SF.SpawnModule
+{
+    /// <summary>
+    /// The system that controls the logic for spawning the player. 
+    /// </summary>
+    public class SpawnSystem : MonoBehaviour
+    {
+        /// <summary>
+        /// The spawned root gameobject of the player.
+        /// </summary>
+        public static GameObject SpawnedPlayer;
+        
+        /// <summary>
+        /// The <see cref="ControllerBody2D"/> of the <see cref="SpawnedPlayer"/>.
+        /// </summary>
+        public static ControllerBody2D SpawnedPlayerController;
+        
+        public static event Action<GameObject> InitialPlayerSpawnHandler;
+
+        /// <summary>
+        /// Tell the game to start the initial spawning of the player when loading up a save file.
+        /// </summary>
+        public static ControllerBody2D OnInitialPlayerSpawn(GameObject playerPrefab)
+        {
+            if (playerPrefab == null)
+                return null;
+
+            SpawnedPlayer = Instantiate(playerPrefab);
+            SpawnedPlayer.transform.position = RoomSystem.CurrentRoom.SpawnedInstance.transform.position;
+            SpawnedPlayerController.TryGetComponent(out SpawnedPlayerController);
+
+            InitialPlayerSpawnHandler?.Invoke(SpawnedPlayer);
+            
+            return SpawnedPlayer.GetComponent<ControllerBody2D>();
+        }
+    }
+}
