2ad14cd474bde42895ed65bb0cbd53bbb3552790
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-02-06 14:20:49 -0600

    Created a base class for CharacterSpawners and made a class for TimedCharacterSpawners.
    Added:
    - Spawn Limit to prevent over spawning of characters.
    - Added the ability for character to spawn on a timer.

---
+++ Runtime/Scripts/Spawn Module/TimedCharacterSpawner.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace SF.SpawnModule
+{
+    public class TimedCharacterSpawner : CharacterSpawner
+    {
+        [SerializeField] private Timer _spawnTimer;
+        [SerializeField] private int _chanceToSpawn = 100;
+
+        private void Start()
+        {
+            _spawnTimer = new Timer(_spawnTimer.Duration, OnSpawnCheck);
+            _           = _spawnTimer.StartTimerAsync();
+        }
+
+        private void OnSpawnCheck()
+        {
+            int randomChance = Random.Range(0, 100);
+			
+            if (randomChance > _chanceToSpawn)
+            {
+                SpawnCharacters();
+            }
+
+            if (_spawnedCount <= _spawnLimit)
+            {
+                _spawnTimer = new Timer(_spawnTimer.Duration, OnSpawnCheck);
+                _           = _spawnTimer.StartTimerAsync();
+            }
+        }
+
+        protected override void OnCharacterDied()
+        {
+            _spawnedCount--;
+
+            if (!_spawnTimer.TimerStopped)
+                return;
+            
+            _spawnTimer = new Timer(_spawnTimer.Duration, OnSpawnCheck);
+            _           = _spawnTimer.StartTimerAsync();
+        }
+    }
+}
