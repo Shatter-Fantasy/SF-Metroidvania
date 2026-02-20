503f23288ebda7d34050fb7c861e77514ed06ee5
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-02-20 13:21:47 -0600

    Removed ITimer interface.
    Removed:
    - Removed the ITimer interface. The Timer class replaces it with a lot more funcitonality.

--- Runtime/Scripts/Utilities/Timing/ITimer.cs
+++
@@ -1,8 +0,0 @@
-namespace SF.Core
-{
-    // TODO: Remove. Replaced by the base class called Timer.
-        public interface ITimer 
-        {
-            public void StartTimer() {}
-        }
-}
