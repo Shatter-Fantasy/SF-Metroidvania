4e0ee56ad9d81c52349daed551a31a4f49c9a766
8BitsPerByte <jonathan.westfall1@gmail.com>
2026-03-15 17:22:16 -0500

    Added checks in code for if the SF Dialogue Graph Tool is set up and valid.

--- Runtime/Scripts/Interactables/DialogueImprov.cs
+++ Runtime/Scripts/Interactables/DialogueImprov.cs
@@ -13,21 +13,29 @@ namespace SF.DialogueModule
     {
         public int ConversationGUID;
         [field:SerializeField] public InteractableMode InteractableMode { get; set; }
+
         
+
         public void Interact() {  }
         public void Interact(PlayerController controller)
         {
+#if SF_DIALOGUE_GRAPH
             DialogueManager.TriggerConversation(ConversationGUID);
+#endif
         }
 
         public void OnTriggerBegin2D(PhysicsEvents.TriggerBeginEvent beginEvent, SFShapeComponent callingShapeComponent)
         {
+#if SF_DIALOGUE_GRAPH
             DialogueManager.TriggerConversation(ConversationGUID);
+#endif
         }
 
         public void OnTriggerEnd2D(PhysicsEvents.TriggerEndEvent endEvent, SFShapeComponent callingShapeComponent)
         {
+#if SF_DIALOGUE_GRAPH
             DialogueManager.StopConversation();
+#endif
         }
     }
 }
